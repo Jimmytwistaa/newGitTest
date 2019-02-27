532c61d0becf394d66d3812ee4678b5d757b44b3
Jimmy Felipe <jimmyfelipe@Jimmys-MBP.attlocal.net>
2019-02-27 15:34:05 -0500

    added apple.java

---
+++ apple.java
@@ -0,0 +1,14 @@
+package apple;
+import java.util.Scanner;
+public class Apple {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int girls, boys, people;
+        girls = 11;
+        boys = 3;
+        people =  girls % boys;
+       System.out.println(people);
+    }
+    
+}
